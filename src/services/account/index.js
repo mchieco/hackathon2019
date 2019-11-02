1821cb118cbd0e4545fcb0742c47e412d84bf4db
Daron McIntosh <mcintoshdaron@gmail.com>
2019-11-02 16:23:24 -0400

    Add buy and sell

--- src/services/account/index.js
+++ src/services/account/index.js
@@ -1,24 +1,34 @@
 import saveService from "../../services/local-storage";
-let stocksOwned = {};
-stocksOwned = saveService.get(stocksOwned);
 
 function buyStock(units, price, symbol) {
-  stocksOwned[symbol].units += units;
-  let boughtUnitsAtPrice = { units: units, price: price, date: new Date.now() };
-  stocksOwned[symbol].history.push(boughtUnitsAtPrice);
+  let stocksOwned = {};
+  stocksOwned = saveService.get("stocksOwned");
+  let stock = stocksOwned[symbol];
+  if (stock == undefined) {
+    stocksOwned[symbol] = {
+      units: units
+    };
+  } else {
+    stocksOwned[symbol].units += units;
+  }
+  saveService.set("stocksOwned", stocksOwned);
 }
 function sellStock(units, price, symbol) {
-  let personOwns = stocksOwned[symbol].units;
-  if (personOwns < units) {
-    return "Error, not enought units";
-  } else if (personOwns == units) {
-    stocksOwned[symbol].units -= units;
-    delete stocksOwned[symbol];
+  let stocksOwned = {};
+  stocksOwned = saveService.get("stocksOwned");
+  let stock = stocksOwned[symbol];
+  if (stock == undefined) {
+    stocksOwned[symbol] = {
+      units: units
+    };
   } else {
     stocksOwned[symbol].units -= units;
   }
+  saveService.set("stocksOwned", stocksOwned);
 }
 function getStocks(symbol = "") {
+  let stocksOwned = {};
+  stocksOwned = saveService.get("stocksOwned");
   if (symbol.length == 1) {
     let units = 0;
     for (const key in stocksOwned) {
@@ -33,4 +43,8 @@ function getStocks(symbol = "") {
   }
 }
 
-export default { buyStock, sellStock, getStocks };
+function get() {
+  return saveService.get("stocksOwned");
+}
+
+export default { buyStock, sellStock, getStocks, get };
--- src/services/local-storage/index.js
+++ src/services/local-storage/index.js
@@ -1,21 +1,25 @@
 function get(key) {
-  return new Promise(resolve => {
-    // Retrieve the object from storage
-    var retrievedObject = localStorage.getItem(key);
-    resolve(JSON.parse(retrievedObject));
-  });
+  // return new Promise(resolve => {
+  // Retrieve the object from storage
+  var retrievedObject = localStorage.getItem(key);
+  if (retrievedObject == null) {
+    return (retrievedObject = {});
+  } else {
+    return JSON.parse(retrievedObject);
+  }
+  // });
 }
 function set(key, value) {
-  return new Promise(resolve => {
-    localStorage.setItem(key, JSON.stringify(value));
-    resolve();
-  });
+  // return new Promise(resolve => {
+  localStorage.setItem(key, JSON.stringify(value));
+  // resolve();
+  // });
 }
 function clear(key) {
-  return new Promise(resolve => {
-    localStorage.clear(key);
-    resolve();
-  });
+  // return new Promise(resolve => {
+  localStorage.clear(key);
+  //   resolve();
+  // });
 }
 
 export default { get, set, clear };
