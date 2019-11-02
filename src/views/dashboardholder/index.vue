e63be264c4a4efea55479f26a83beb0c42479415
Joe Passanante <j.e.passanante@gmail.com>
2019-11-02 11:16:45 -0400

    add vue files

---
+++ src/views/dashboardholder/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-model="primaryDrawer.model"
+      :clipped="primaryDrawer.clipped"
+      :floating="primaryDrawer.floating"
+      :mini-variant="primaryDrawer.mini"
+      :permanent="primaryDrawer.type === 'permanent'"
+      :temporary="primaryDrawer.type === 'temporary'"
+      app
+      overflow
+    />
+
+    <v-app-bar :clipped-left="primaryDrawer.clipped" app>
+      <v-app-bar-nav-icon
+        v-if="primaryDrawer.type !== 'permanent'"
+        @click.stop="primaryDrawer.model = !primaryDrawer.model"
+      />
+      <v-toolbar-title>Learn to Trade</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+
+    <v-footer :inset="footer.inset" app>
+      <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    drawers: ["Default (no property)", "Permanent", "Temporary"],
+    primaryDrawer: {
+      model: null,
+      type: "default (no property)",
+      clipped: true,
+      floating: false,
+      mini: false
+    },
+    footer: {
+      inset: false
+    }
+  })
+};
+</script>
