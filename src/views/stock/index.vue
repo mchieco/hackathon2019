8790d19572bd52870775d68399f29ac02217a648
Joe Passanante <j.e.passanante@gmail.com>
2019-11-02 16:18:38 -0400

    do it

--- src/views/stock/index.vue
+++ src/views/stock/index.vue
@@ -25,6 +25,28 @@
         <v-btn rounded large color="red" style="color: white;">Sell</v-btn>
       </div>
     </v-layout>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="Stocks to Buy" required></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Buy</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
